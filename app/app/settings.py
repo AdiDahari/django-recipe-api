523cd16d71176d138670e0b2d67963c352e61258
Adi Dahari <dahariadi16@gmail.com>
2024-02-22 20:38:19 +0200

    Add user app and include user URLs in main app

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "core",
     "rest_framework",
     "drf_spectacular",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -135,5 +136,4 @@ AUTH_USER_MODEL = "core.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-
 }
---
+++ app/user/urls.py
@@ -0,0 +1,14 @@
+"""
+Url patterns for user app
+"""
+
+from django.urls import path
+
+from user import views
+
+
+app_name = "user"
+
+urlpatterns = [
+    path("create/", views.CreateUserView.as_view(), name="create"),
+]
