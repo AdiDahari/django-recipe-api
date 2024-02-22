e530da8c1141a909b793aab836e196431625a476
Adi Dahari <dahariadi16@gmail.com>
2024-02-22 14:19:34 +0200

    Refactor user creation methods and add tests

--- app/core/models.py
+++ app/core/models.py
@@ -21,8 +21,26 @@ class UserManager(BaseUserManager):
         """
         if not email:
             raise ValueError("Users must have an email address.")
-        user = self.model(email=email, **extra_fields)
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Create and save a new superuser.
+        """
+        user = self.create_user(
+            email,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
